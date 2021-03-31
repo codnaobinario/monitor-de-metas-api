60a9a16c6d45daeeb6459f81c68f8828dabb53b9
Mauricio Nunes <mnunes@vialink.com.br>
2021-03-31 01:57:16 -0300

    fix path wait

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -2,7 +2,9 @@
 
 chown -R www-data: /monitor-de-metas-api/
 
-wget https://github.com/ufoscout/docker-compose-wait/releases/download/2.8.0/wait /wait
+wget https://github.com/ufoscout/docker-compose-wait/releases/download/2.8.0/wait waitingfor
+
+cp waitingfor /usr/local/bin
 
 sed -i -e "s/root %%NGINX_ROOT%%;/root \/monitor-de-metas-api\/public;/g" /etc/nginx/sites-available/default.conf
 sed -i -e "s/'default'\s*=>\s*'mysql'/'default' => '${DBDRIVE}'/g" app/config/database.php
@@ -12,9 +14,9 @@ sed -i  -e "s/'username'\s*=>\s*'wp'/'username' => '${DBUSER}'/g" app/config/dat
 sed -i  -e "s/'password'\s*=>\s*'wp'/'password' => '${DBPASS}'/g" app/config/database.php
 sed -i  -e "s/'prefix'\s*=>\s*''/'prefix' => '${DBPREFIX}'/g" app/config/database.php
 
-sh -c "/wait && php artisan migrate --env=production"
+sh -c "waitingfor && php artisan migrate --env=production"
 
-sh -c "/wait && php artisan db:seed"
+sh -c "waitingfor && php artisan db:seed"
 
 chmod -R 777 /monitor-de-metas-api/app/storage
 /etc/init.d/php5.6-fpm start
